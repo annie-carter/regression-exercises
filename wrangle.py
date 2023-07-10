849145d88537ea626b5149562456798bae6fd17c
annie-carter <annie.carter831@gmail.com>
2023-07-10 07:49:50 -0500

    added and updated

--- wrangle.py
+++ wrangle.py
@@ -3,12 +3,15 @@ import numpy as np
 import os
 from env import hostname, user, password
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import warnings
+warnings.filterwarnings("ignore")
 
 def get_connection(db, user=user, hostname =hostname, password=password):
     return f'mysql+pymysql://{user}:{password}@{hostname}/{db}'
 
 def zillow_data():
-    #we save first before editing so that we still have the original on hand
+    #Save before editing so that we still have the original on hand
     filename = "zillow.csv"
     if os.path.isfile(filename):
         return pd.read_csv(filename)
@@ -16,7 +19,7 @@ def zillow_data():
         return get_zillow_data()
 
 def get_zillow_data():
-    #this sql code answers Q1 E2 from regressions lesson
+    #this sql code for answers 
     sql_query = """SELECT
     bedroomcnt,
     bathroomcnt,
@@ -36,6 +39,23 @@ WHERE
     df = pd.read_sql(sql_query, get_connection('zillow'))
     return df
 
+def prep_zillow(df):
+    '''
+    Used for scaling ex. This function takes in the zillow df
+    then the data is cleaned and returned
+    '''
+    df = df.drop('propertylandusetypeid', axis=1)
+    #change column names to be more readable
+    df = df.rename(columns={'bedroomcnt':'bedrooms','bathroomcnt':'bathrooms', 'calculatedfinishedsquarefeet':'sqft', 'taxvaluedollarcnt':'home_value', 'taxamount':'sale_tax', 'yearbuilt':'year_built'})
+
+    #drop null values- at most there were 9000 nulls (this is only 0.5% of 2.1M)
+    df = df.dropna()
+
+    #drop duplicates
+    df.drop_duplicates(inplace=True)
+   
+    return df
+
 def drop_propertylandusetypeid(df):
     df = df.drop('propertylandusetypeid', axis=1)
     return df
@@ -49,32 +69,26 @@ def drop_nobed_nobath(df):
     return df
 
 def squish_outliers(df):
-    #take out properties that are beyond a reasonable price
+    #take out outliers
     df = df[df.bathroomcnt <= 6]
-    
     df = df[df.bedroomcnt <= 6]
-
     df = df[df.taxvaluedollarcnt < 2_000_000]
-
     return df
 
 def wrangled_zillow(df):
     # df = drop_propertylandusetypeid(df)
 
     df = drop_nulls(df)
-
     df = drop_nobed_nobath(df)
-
     df = squish_outliers(df)
-
     df.to_csv("zillow.csv", index=False)
-
     return df
 
-def train_validate_test(df):
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.fips)
+def split_zillow(df):
+    #df = train_validate_test(df)
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=123, stratify=df.fips)
     #stratify is used for categorical data
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate.fips)
-    #I can stratify on county if i want an even distribution of it in my train validate test models
+    train, validate = train_test_split(train_validate, test_size=0.25, random_state=123, stratify=train_validate.fips)
+    #Stratified by county for an even distribution on the train validate test data sets
     return train, validate, test
     #go back and stratify properly on fips
